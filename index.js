cde66d9e875f277e8b01f52a9b8d5415488526ee
impac <impacevanend@gmail.com>
2021-11-29 20:18:20 -0500

    se agrega tipo de datos,resolver...

---
+++ index.js
@@ -0,0 +1,74 @@
+import { ApolloServer, gql } from "apollo-server"; 
+
+const pesons = [
+    {name: "Midu",
+    phone: "034-1234567",
+    street: "Calle Frontend",
+    city: "Barcelona",
+    id:"3d594650-3436-11e9-bc57-8b80ba54c431"
+    },
+
+    {name: "Youseff",
+    phone: "044-123456",
+    street: "Avenida Fullstack",
+    city: "Mataro",
+    id:"3d599470-3436-11e9-bc57-8b80ba54c431"
+    },
+
+    {name: "Itzi",
+    street: "Pasaje Testing",
+    city: "Ibiza",
+    id:"3d599471-3436-11e9-bc57-8b80ba54c431"
+    },
+]
+
+// description of data:
+
+const typeDefs = gql`
+    type Address {
+        street: String
+        city: String
+    }
+    type Person{
+        name: String!
+        phone: String
+        street: String!
+        city: String!
+        address: Address!
+        id: ID!
+    }
+
+    type Query{
+        pesonCount: Int!
+        allPersons: [Person]!
+        findPerson(name:String!): Person
+    }
+    `
+
+    const resolvers ={
+        Query: {
+            pesonCount:() => pesons.length,
+            allPersons:() => pesons,
+            findPerson: (root, args)=>{
+                const {name} = args
+                return pesons.find(person => person.name === name)
+            }
+        },
+        Person: {
+            address: (root) => {
+                return {
+                    street: root.street,
+                    city: root.city
+                }
+            }
+        }
+    }
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+    })
+
+    server.listen().then(({ url }) => {
+        console.log(`Server ready at ${url}`)
+    })
